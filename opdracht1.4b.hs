c5fd3890f32d6bb0d2c81d82c8da388f10cab842
tobiasdb-ai <tobiasdebildt@gmail.com>
2023-02-16 15:59:16 +0100

    AI made 1.4b

--- opdracht1.4b.hs
+++ opdracht1.4b.hs
@@ -21,3 +21,22 @@ russianPeasant a b = go a b 0
       | a == 0 = acc
       | odd a = go (shiftR a 1) (shiftL b 1) (acc + b)
       | otherwise = go (shiftR a 1) (shiftL b 1) acc
+
+
+
+{--
+The go function in the russianPeasant implementation is a recursive helper function that performs the actual Russian Peasant multiplication.
+
+The go function takes three arguments:
+
+a: the first number to be multiplied.
+b: the second number to be multiplied.
+acc: the accumulated product of a and b so far.
+The function uses the following algorithm to compute the product of a and b:
+
+If a is 0, return acc. This is the base case of the recursion.
+If a is odd (i.e., the least significant bit of a is 1), add b to acc, and then halve a and double b. This is equivalent to shifting a one bit to the right and shifting b one bit to the left, and adding b to acc.
+If a is even, halve a and double b, and return acc unchanged. This is equivalent to shifting a one bit to the right and shifting b one bit to the left.
+The function keeps recursively calling itself until a becomes 0, which is the base case of the recursion. At that point, it returns the accumulated product acc.
+
+The go function is tail-recursive, which means that it can be optimized by the Haskell compiler to use constant stack space, even for large inputs.--}
