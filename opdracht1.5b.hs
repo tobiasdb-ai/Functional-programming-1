f6ea0e69b7c75fec422d7a77771047ba844ddfd6
tdregmans <tdregmans+github@protonmail.com>
2023-02-23 12:09:01 +0100

    Opdracht 4b en 5b gemaakt.

--- opdracht1.5b.hs
+++ opdracht1.5b.hs
@@ -4,6 +4,8 @@ import Data.Bits
 -- https://hackage.haskell.org/package/base-4.17.0.0/docs/Data-Bits.html
 
 fastpow :: Integer -> Integer -> Integer
-fastpow x 0 = 1
-fastpow 0 p = 0
-fastpow x p = 
+fastpow x p
+  | p == 0 = 1
+  | even p = c
+  | otherwise = x * c
+  where c = fastpow (x * x) (shiftR p 1)
