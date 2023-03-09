09a007edde1d7429c611cfb24372638d0d33a7a2
tdregmans <tdregmans+github@protonmail.com>
2023-03-09 15:24:00 +0100

    opdracht 2.1

--- opdracht2.1a.hs
+++ opdracht2.1a.hs
@@ -1,10 +1,5 @@
 module Opdracht2Point1a where
 
-import Data.List ( nub, (\\) )
 euclid :: Integer -> Integer -> Integer
-euclid x y = 
-    let list = [z|z<-[1..x], x `mod` z == 0]++[z|z<-[1..y], y `mod` z == 0]
-    in maximum ((list) \\ (nub list))
-
-
-
+euclid x y =
+    if y `rem` x == 0 then x else euclid (y `rem` x) x
--- opdracht2.1b.hs
+++ opdracht2.1b.hs
@@ -1,8 +1,16 @@
 module Opdracht2Point1b where
 
+-- egcd :: Integer -> Integer -> (Integer,Integer,Integer)
+-- egcd 0 b = (b, 0 ,1)
+-- egcd a b =
+--     let (g, s, t) = egcd (b `mod` a) a
+--     in (g, t - (b `div` a) * s, s)
+
 egcd :: Integer -> Integer -> (Integer,Integer,Integer)
 egcd 0 b = (b, 0 ,1)
 egcd a b =
     let (g, s, t) = egcd (b `mod` a) a
-    in (g, t - (b `div` a) * s, s)
+    in (g, t + (b `div` a) * s, s)
+
+-- aanpassing is `-` vervangen voor een `+`
 
