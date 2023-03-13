df8fb6c9406010b829eba76f277b415d9c854425
tdregmans <tdregmans+github@protonmail.com>
2023-03-13 10:08:16 +0100

    opdracht 2.2 cleaned

--- opdracht2.2.hs
+++ opdracht2.2.hs
@@ -1,44 +1,28 @@
 module Opdracht2Point2 where
-import Control.Arrow (Arrow(first))
-import System.Win32 (RegInfoKey(sec_len))
-
-p = 7
-q = 13
 
 -- Begin Code from opdracht2.1a.hs
-
 euclid :: Integer -> Integer -> Integer
 euclid x y =
     if y `rem` x == 0 then x else euclid (y `rem` x) x
-
 -- End Code from opdracht2.1a.hs
 
 m :: Integer -> Integer -> Integer
 m p q = p * q
 
--- m = 50737
-
 eulersTotient :: Integer -> Integer -> Integer
 eulersTotient p q = (p - 1) * (q - 1)
 
--- eulersTotient = 50176
-
 e :: Integer -> Integer -> Integer
--- e :: Integer -> Integer -> [Integer]
 e p q =  [possibleE|possibleE<-[1..eulersTotient p q -1], euclid possibleE (eulersTotient p q) == 1] !! 1
-
--- e = 55775
+-- the `!! 1` suffix means that the second from the list is returned
 
 -- `In order for the congruence relation a ≡ b (mod n) to hold, n must divide a - b`
-
--- a ­≅ b `mod` c
--- congruency :: Integer -> Integer -> Integer -> Bool
-congruency :: Integral a => a -> a -> a -> Bool
-congruency a b c = rem (a - b) c == 0
+congruency :: Integer -> Integer -> Integer -> Bool
+congruency a b n = rem (a - b) n == 0
 
 -- d :: Integer -> Integer -> Integer
-d e eulersTotient = [dPossiblity|dPossiblity<-[1..100000], congruency (dPossiblity*e) 1 eulersTotient]
--- d e eulersTotient = [d|d<-[1..100000], (((d*e) - 1) `div` eulersTotient) `mod` 0]
+d e eulersTotient = [dPossiblity|dPossiblity<-[1..100000], congruency (dPossiblity*e) 1 eulersTotient] !! 1
+-- the `!! 1` suffix means that the second from the list is returned
 
 
 
