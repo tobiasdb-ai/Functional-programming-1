b2eb9c1c57f3882c4a912075bd534c97e2c5e731
tdregmans <tdregmans+github@protonmail.com>
2023-03-13 10:15:24 +0100

    RSA encrypt and decrypt written

--- opdracht2.3a.hs
+++ opdracht2.3a.hs
@@ -1,4 +1,4 @@
 module Opdracht2Point3a where
 
 rsaencrypt :: (Integer, Integer) -> Integer -> Integer
-rsaencrypt (e, m) x
+rsaencrypt (e, m) x = (x ^ e) `mod` m
--- opdracht2.3b.hs
+++ opdracht2.3b.hs
@@ -1,4 +1,4 @@
 module Opdracht2Point3b where
 
 rsadecrypt :: (Integer, Integer) -> Integer -> Integer
-rsadecrypt (d, m) x
+rsadecrypt (d, m) x = (x ^ d) `mod` m
