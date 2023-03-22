b06d3e2ddff25cd32fafbd85b4d7deb727ca2da2
tdregmans <tdregmans+github@protonmail.com>
2023-03-22 10:42:10 +0100

    corrected erors

--- opdracht2.hs
+++ opdracht2.hs
@@ -28,14 +28,14 @@ eulersTotient :: Integer -> Integer -> Integer
 eulersTotient p q = (p - 1) * (q - 1)
 
 e :: Integer -> Integer -> Integer
-e p q =  [e|e<-[1..eulersTotient p q -1], euclid e (eulersTotient p q) == 1] !! 1
+e p q = [e|e<-[1..eulersTotient p q -1], euclid e (eulersTotient p q) == 1] !! 1
 -- the `!! 1` suffix means that the second from the list is returned
 
 -- `In order for the congruence relation a ≡ b (mod n) to hold, n must divide a - b`
 congruency :: Integer -> Integer -> Integer -> Bool
 congruency a b n = rem (a - b) n == 0
 
--- d :: Integer -> Integer -> Integer
+d :: Integer -> Integer -> Integer
 d e eulersTotient = [d|d<-[1..100000], congruency (d*e) 1 eulersTotient] !! 1
 -- the `!! 1` suffix means that the second from the list is returned
 
