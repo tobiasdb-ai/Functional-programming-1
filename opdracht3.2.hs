a29cbb34b821b5674c603f4c2a61c55992a582db
tdregmans <tdregmans+github@protonmail.com>
2023-03-23 23:54:43 +0100

    first version 3.2; Only not right: can only work with chars and requires additonal function

--- opdracht3.2.hs
+++ opdracht3.2.hs
@@ -2,6 +2,16 @@ module Opdracht3Point2 where
 
 import Data.List
 
-dubbelen :: [a] -> [a]
-dubbelen s
+lengthGreaterThanOne x = if length x > 1 then True else False
 
+-- dubbelen :: [a] -> [a]
+
+
+dubbelen :: Ord a => [a] -> [a]
+dubbelen s = (concat (filter lengthGreaterThanOne ((group (sort s)))))
+
+
+-- zip s [1..]
+-- ghci> concat (group (sort s))
+
+-- (zip ((group (sort s))) (map (length) (group (sort s))))
