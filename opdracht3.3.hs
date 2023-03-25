da3bba0a990d9e77030c5f73e2749a10059e5954
tdregmans <tdregmans+github@protonmail.com>
2023-03-25 21:41:52 +0100

    3.3 techically works now

--- opdracht3.3.hs
+++ opdracht3.3.hs
@@ -6,7 +6,7 @@ import Data.List
 poss :: [(Integer, Integer, Integer, Integer, Integer)]
 poss =  [(a,b,c,d,e)|a<-[1..6],b<-[1..6],c<-[1..6],d<-[1..6],e<-[1..6]]
 
-con = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2]]
+con = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2],[6,5,4,3,1]]
 
 -- phase1 :: Ord b => [b] -> [(Int, b)]
 phase1 x = zip (map length (group (sort x))) (nub ((sort x))) -- results in [(no. of indices, eyes)]
@@ -15,19 +15,21 @@ phase1 x = zip (map length (group (sort x))) (nub ((sort x))) -- results in [(no
 
 -- phase2 x = filter (\(p,_) -> p>1) (phase1 x)
 phase2 x y = filter (\(p,_) -> p==y) (phase1 x)
-phase3 x y = any (\(p,_) -> p==y) (phase1 x)
+-- phase3 x y = any (\(p,_) -> p==y) (phase1 x)
 
 -- (\x -> phase3 x y) == (\x -> length (phase2 x y) == 1)
 
+straightFilter x = if sort x == [1,2,3,4,5] || sort x == [2,3,4,5,6] then True else False
 
-poker list = filter (\x -> phase3 x 5) list
-fourOfAKind list = filter (\x -> phase3 x 4) list
-threeOfAKind list = filter (\x -> phase3 x 3) list
-fullHouse list = filter (\x -> phase3 x 4) list
+
+poker list = filter (\x -> length (phase2 x 5) == 1) list
+fourOfAKind list = filter (\x -> length (phase2 x 4) == 1) list
+threeOfAKind list = filter (\x -> length (phase2 x 3) == 1 && length (phase2 x 2) == 0) list
+fullHouse list = filter (\x -> length (phase2 x 2) == 1 && length (phase2 x 3) == 1) list
 twoPair list = filter (\x -> length (phase2 x 2) == 2) list
-onePair list = filter (\x -> length (phase2 x 2) == 2) list
--- straight = 
--- bust = 
+onePair list = filter (\x -> length (phase2 x 2) == 1 && length (phase2 x 3) == 0) list
+straight list = filter straightFilter list
+bust list = filter (\x -> length ((poker [x]) ++ (fourOfAKind [x]) ++ (threeOfAKind [x]) ++ (fullHouse [x]) ++ (twoPair [x]) ++ (onePair [x]) ++ (straight [x])) == 0) list
 
 
 -- phase2 x = filter () (phase1 x)
