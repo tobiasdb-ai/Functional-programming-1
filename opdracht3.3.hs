52e51303fabfa1846ce00a50072c6729944d7f5a
tdregmans <tdregmans+github@protonmail.com>
2023-03-25 22:01:11 +0100

    possible end version

--- opdracht3.3.hs
+++ opdracht3.3.hs
@@ -2,34 +2,26 @@ module Opdracht3Point3 where
 
 import Data.List
 
--- poss :: [()]
-poss :: [(Integer, Integer, Integer, Integer, Integer)]
-poss =  [(a,b,c,d,e)|a<-[1..6],b<-[1..6],c<-[1..6],d<-[1..6],e<-[1..6]]
+allPossibilities :: [(Integer, Integer, Integer, Integer, Integer)]
+allPossibilities =  [(a,b,c,d,e)|a<-[1..6],b<-[1..6],c<-[1..6],d<-[1..6],e<-[1..6]]
 
-con = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2],[6,5,4,3,1]]
+example = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2],[6,5,4,3,1]]
 
--- phase1 :: Ord b => [b] -> [(Int, b)]
-phase1 x = zip (map length (group (sort x))) (nub ((sort x))) -- results in [(no. of indices, eyes)]
+noIndices :: Ord b => [b] -> [(Int, b)]
+noIndices x = zip (map length (group (sort x))) (nub ((sort x))) -- results in [(no. of indices, eyes)]
 
--- equ x = if x == 1 then True else False
 
--- phase2 x = filter (\(p,_) -> p>1) (phase1 x)
-phase2 x y = filter (\(p,_) -> p==y) (phase1 x)
--- phase3 x y = any (\(p,_) -> p==y) (phase1 x)
-
--- (\x -> phase3 x y) == (\x -> length (phase2 x y) == 1)
+filterIndices :: Ord b => [b] -> Int -> [(Int, b)]
+filterIndices x y = filter (\(p,_) -> p==y) (noIndices x)
 
 straightFilter x = if sort x == [1,2,3,4,5] || sort x == [2,3,4,5,6] then True else False
 
-
-poker list = filter (\x -> length (phase2 x 5) == 1) list
-fourOfAKind list = filter (\x -> length (phase2 x 4) == 1) list
-threeOfAKind list = filter (\x -> length (phase2 x 3) == 1 && length (phase2 x 2) == 0) list
-fullHouse list = filter (\x -> length (phase2 x 2) == 1 && length (phase2 x 3) == 1) list
-twoPair list = filter (\x -> length (phase2 x 2) == 2) list
-onePair list = filter (\x -> length (phase2 x 2) == 1 && length (phase2 x 3) == 0) list
+poker list = filter (\x -> length (filterIndices x 5) == 1) list
+fourOfAKind list = filter (\x -> length (filterIndices x 4) == 1) list
+threeOfAKind list = filter (\x -> length (filterIndices x 3) == 1 && length (filterIndices x 2) == 0) list
+fullHouse list = filter (\x -> length (filterIndices x 2) == 1 && length (filterIndices x 3) == 1) list
+twoPair list = filter (\x -> length (filterIndices x 2) == 2) list
+onePair list = filter (\x -> length (filterIndices x 2) == 1 && length (filterIndices x 3) == 0) list
 straight list = filter straightFilter list
 bust list = filter (\x -> length ((poker [x]) ++ (fourOfAKind [x]) ++ (threeOfAKind [x]) ++ (fullHouse [x]) ++ (twoPair [x]) ++ (onePair [x]) ++ (straight [x])) == 0) list
 
-
--- phase2 x = filter () (phase1 x)
--- opdracht3.hs
+++ opdracht3.hs
@@ -22,3 +22,26 @@ dubbelen :: Eq a => [a] -> [a]
 dubbelen s = nub (s \\ (nub s))
 
 -- Opdracht 3.3
+
+example :: [[Integer]]
+example = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2],[6,5,4,3,1]]
+
+noIndices :: Ord b => [b] -> [(Int, b)]
+noIndices x = zip (map length (group (sort x))) (nub ((sort x))) -- results in [(no. of indices, eyes)]
+
+
+filterIndices :: Ord b => [b] -> Int -> [(Int, b)]
+filterIndices x y = filter (\(p,_) -> p==y) (noIndices x) -- results in [(y, eyes)]
+
+-- separate function/filter for straight 
+straightFilter x = if sort x == [1..5] || sort x == [2..6] then True else False
+
+poker list = filter (\x -> length (filterIndices x 5) == 1) list
+fourOfAKind list = filter (\x -> length (filterIndices x 4) == 1) list
+threeOfAKind list = filter (\x -> length (filterIndices x 3) == 1 && length (filterIndices x 2) == 0) list
+fullHouse list = filter (\x -> length (filterIndices x 2) == 1 && length (filterIndices x 3) == 1) list
+twoPair list = filter (\x -> length (filterIndices x 2) == 2) list
+onePair list = filter (\x -> length (filterIndices x 2) == 1 && length (filterIndices x 3) == 0) list
+straight list = filter straightFilter list
+bust list = filter (\x -> length ((poker [x]) ++ (fourOfAKind [x]) ++ (threeOfAKind [x]) ++ (fullHouse [x]) ++ (twoPair [x]) ++ (onePair [x]) ++ (straight [x])) == 0) list
+
